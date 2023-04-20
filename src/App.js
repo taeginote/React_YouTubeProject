2b7009154e08aa0e288716a99b6b5f0535313b34
taeginote <ab315601@naver.com>
2023-04-20 20:23:30 +0900

    ✨Feat : json 폴더명 수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,24 @@
 function App() {
+	/*
+    mock데이터
+    channel.json ===  채널 상세내용
+    popular.json === 핫트렌드 비디오들
+    related.json === 연관된 비디오 검색
+    search.json=== 키워드 검색
+  */
+
+	/*
+  router 
+  url 
+  1.  
+  http://localhost:3000/videos 일때 
+  videos라는 h1태그 보여주기
+  2. 
+  http://localhost:3000/videos/사용자가 입력한 search값
+  3. video 선택했을때
+  http://localhost:3000/videos/watch/비디어에 특정 아이디값
+  */
+	4
 	return <></>
 }
 
